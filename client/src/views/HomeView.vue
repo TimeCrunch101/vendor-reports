3e9950fea2b8563bbf42ceb97a6ee21cb9a1dd97
timecrunch101 <apaallen101@gmail.com>
2024-10-03 14:14:23 -0400

    chore: cleanup

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,18 +1,7 @@
 <script setup>
-import {useAuthStore} from "../stores/auth.js"
-import {useRouter} from "vue-router"
-const router = useRouter()
-const auth = useAuthStore()
-
-
-const logout = () => {
-    auth.logout()
-    router.push("/login")
-}
 
 </script>
 
 <template>
-<h1>HELLO WORLD</h1>
-<button @click="logout()">LOGOUT</button>
+
 </template>
