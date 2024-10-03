984791e290e56d510053099597640b0f75afc8a2
timecrunch101 <apaallen101@gmail.com>
2024-10-03 14:14:37 -0400

    wip: vendor view

---
+++ client/src/views/Vendor.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { useRoute, useRouter } from 'vue-router';
+import { onMounted, ref } from "vue";
+import axios from 'axios';
+
+const route = useRoute()
+const router = useRouter()
+
+
+
+onMounted(() => {
+
+})
+
+</script>
+
+<template>
+
+<nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><router-link to="/vendors">Vendors</router-link></li>
+    <li class="breadcrumb-item active"><a href="#">Library</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Data</li>
+  </ol>
+</nav>
+
+
+
+
+
+</template>
+
+<style scoped>
+
+</style>
