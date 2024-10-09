f370443a0037d17a41416f1192d1923c5d0abdbf
timecrunch101 <apaallen101@gmail.com>
2024-10-09 09:00:34 -0400

    feat: sets 0 to input fields

--- client/src/components/VendorSales.vue
+++ client/src/components/VendorSales.vue
@@ -39,7 +39,13 @@ const getSales = () => {
             Authorization: `Bearer ${token.value}`
         }
     }).then((res) => {
-        set.sales = res.data.vendorSales
+        const salesFromDB = res.data.vendorSales
+        salesFromDB.forEach((element, index) => {
+            if (element.qty_sold === null) {
+                salesFromDB[index].qty_sold = 0
+            }
+        });
+        set.sales = salesFromDB
     }).catch((err) => {
         console.error(err.response.data)
     })
@@ -94,7 +100,7 @@ const zeroize = () => {
     normalizeMonth()
     set.selectedYear = currentYear.value
     sales.value.forEach(sale => {
-        sale.qty_sold = null        
+        sale.qty_sold = 0        
     });
 }
 
@@ -105,59 +111,51 @@ onMounted(() => {
 
 </script>
 <template>
-
-<button @click="saveSales()" class="btn btn-primary mt-3 mb-3">Save</button>
-<div class="d-flex">
-    <select class="form-select mb-3" aria-label="Default select example" v-model="selectedYear" style="width: max-content;">
-        <option v-for="(num, index) in 10" :id="index" :value="(currentYear+1)-num">{{ (currentYear+1)-num }}</option>
-    </select>
-    <select class="form-select mb-3" aria-label="Default select example" v-model="selectedMonth" style="width: max-content;">
-        <option value="01">January</option>
-        <option value="02">February</option>
-        <option value="03">March</option>
-        <option value="04">April</option>
-        <option value="05">May</option>
-        <option value="06">June</option>
-        <option value="07">July</option>
-        <option value="08">August</option>
-        <option value="09">September</option>
-        <option value="10">October</option>
-        <option value="11">November</option>
-        <option value="12">December</option>
-    </select>
-</div>
-
-<div class="table-responsive">
-    <table class="table table-hover table-bordered table-striped table-sm">
-        <caption>EOM Sales - {{ props.vendorName }}</caption>
-    <thead>
-        <tr>
-          <th scope="col">Item</th>
-          <th scope="col">Price</th>
-          <th scope="col">Qty OH</th>
-          <th scope="col">Sold</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr v-for="(item, index) in sales" :key="item.id">
-          <th scope="row">{{ item.item_name }}</th>
-          <th>${{ item.price }}</th>
-          <th>{{ item.qty }}</th>
-          <td><input type="number" min="0" :name="`qty_sold-${index}`" :id="`qty-sold-${index}`" v-model="sales[index].qty_sold"></td>
-        </tr>
-    </tbody>
-    </table>
-</div>
-
-
-
-
+    <div class="d-flex mt-3 mb-3">
+        <button style="margin-right: 1em;" @click="saveSales()" class="btn btn-primary">Save Sales Report</button>
+        <select class="form-select" aria-label="Default select example" v-model="selectedYear" style="width: max-content;">
+            <option v-for="(num, index) in 10" :id="index" :value="(currentYear+1)-num">{{ (currentYear+1)-num }}</option>
+        </select>
+        <select class="form-select" aria-label="Default select example" v-model="selectedMonth" style="width: max-content;">
+            <option value="01">January</option>
+            <option value="02">February</option>
+            <option value="03">March</option>
+            <option value="04">April</option>
+            <option value="05">May</option>
+            <option value="06">June</option>
+            <option value="07">July</option>
+            <option value="08">August</option>
+            <option value="09">September</option>
+            <option value="10">October</option>
+            <option value="11">November</option>
+            <option value="12">December</option>
+        </select>
+        
+    </div>
+    <div class="table-responsive">
+        <table class="table table-hover table-bordered table-striped table-sm">
+            <caption>EOM Sales - {{ props.vendorName }}</caption>
+        <thead>
+            <tr>
+            <th scope="col">Item</th>
+            <th scope="col">Price</th>
+            <th scope="col">Qty OH</th>
+            <th scope="col">Sold</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(item, index) in sales" :key="item.id">
+            <th scope="row">{{ item.item_name }}</th>
+            <th>${{ item.price }}</th>
+            <th>{{ item.qty }}</th>
+            <td><input class="form-control form-control-sm" type="number" min="0" :name="`qty_sold-${index}`" :id="`qty-sold-${index}`" v-model="sales[index].qty_sold"></td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
 </template>
-
-
-
 <style scoped>
-
-
-
+.table-responsive {
+    max-width: 70%;
+}
 </style>
